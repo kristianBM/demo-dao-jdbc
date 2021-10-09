5a1e599eeace243a6ec2603f048af8f3d8bcb852
Kristian Mello <kristian.bonfim@hotmail.com>
2021-10-09 18:18:53 -0300

    SellerDaoJDBC with FindById = ok and class DB fixed

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,18 +1,16 @@
 package application;
 
-import java.util.Date;
-
-import model.entities.Department;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-	Department obj = new Department(1, "books");
-			System.out.println(obj);
+	SellerDao sellerDao = DaoFactory.createSellerDao();
 	
-	Seller seller = new Seller(4858, "Marcos Claudio", "marcos.claudio@hotmail.com", new Date(), 2000.00, obj);
-			System.out.println(seller);
+	Seller seller = sellerDao.findById(3);
+	System.out.println(seller);
 	}
 }

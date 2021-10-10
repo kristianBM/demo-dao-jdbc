cc09f1e0b9ed6d603e6e2ec21e6f65c7efd4a50a
Kristian Mello <kristian.bonfim@hotmail.com>
2021-10-09 21:50:15 -0300

    seller insert implemented!

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -32,5 +33,12 @@ public class Program {
 		for (Seller all : findAll) {
 			System.out.println(all);
 		}
+		
+		System.out.println();
+		
+		System.out.println("=== TESTE 4: seller insert===");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 5000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! Id: " + newSeller.getId());
 	}
 }
